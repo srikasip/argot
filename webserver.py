5665328d64cef7e3d5788998c77457003c1ffc8a
srikasip <srikasip@gmail.com>
2016-12-20 13:21:08 -0500

    Just adding a quick if block to manage the different pages as we go forward.

--- webserver.py
+++ webserver.py
@@ -1,13 +1,20 @@
 def app(environ, start_response):
-  print "SUBPAGE: " + environ["PATH_INFO"]
-  print "PARAMETERS: " + environ["QUERY_STRING"]
-  data = "Hello, World!\n"
-  
+  subpaths = environ["PATH_INFO"].split('/')
+  params = environ["QUERY_STRING"].split('&')
+  print params
 
+  mainPath = ""
+  if len(subpaths)>=2:
+    mainPath = subpaths[1]
+
+  if mainPath == "canvas":
+    data = "You're in the canvas!  We are working on it!\n"  
+  else:
+    data = "Hello, World!\n"
+  
   start_response("200 OK", [
       ("Content-Type", "text/plain"),
       ("Content-Length", str(len(data)))
   ])
-
 
   return iter([data])
