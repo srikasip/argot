5bd6ac94acac39f5ee6d63c00112af7beee343f5
srikasip <srikasip@gmail.com>
2016-12-20 13:04:09 -0500

    Finally got the python thing to work with paramters and subpages.  This was using gunicorn.  Thank you gunicorn

--- webserver.py
+++ webserver.py
@@ -1,7 +1,13 @@
 def app(environ, start_response):
-   data = "Hello, World!\n"
-   start_response("200 OK", [
-       ("Content-Type", "text/plain"),
-       ("Content-Length", str(len(data)))
-   ])
-   return iter([data])
+  print "SUBPAGE: " + environ["PATH_INFO"]
+  print "PARAMETERS: " + environ["QUERY_STRING"]
+  data = "Hello, World!\n"
+  
+
+  start_response("200 OK", [
+      ("Content-Type", "text/plain"),
+      ("Content-Length", str(len(data)))
+  ])
+
+
+  return iter([data])
