5bbbc5ea36ea836a00c617549eb26e8eb2b99283
srikasip <srikasip@gmail.com>
2016-12-20 12:11:37 -0500

    Working on debugging the problem, at least

--- webserver.py
+++ webserver.py
@@ -13,6 +13,7 @@ print 'Serving HTTP on port %s ...' % prt
 while True:
     client_connection, client_address = listen_socket.accept()
     request = client_connection.recv(1024)
+    print "True: "
     print request
 
     http_response = """\
