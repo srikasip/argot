4faece455a32809c3c538c681876069b0b6e9d7f
srikasip <srikasip@gmail.com>
2016-12-20 11:30:18 -0500

    Another mod

--- webserver.py
+++ webserver.py
@@ -2,13 +2,14 @@ import socket
 import os
 
 HOST = '0.0.0.0'
-PORT = int(os.environ.get('PORT', '8888'))
+prt = int(os.environ.get('PORT', '8888'))
 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listen_socket.bind((HOST, PORT))
+listen_socket.bind((HOST, prt))
 listen_socket.listen(1)
-print 'Serving HTTP on port %s ...' % PORT
+
+print 'Serving HTTP on port %s ...' % prt
 while True:
     client_connection, client_address = listen_socket.accept()
     request = client_connection.recv(1024)
