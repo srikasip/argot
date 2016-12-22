cd8efb6d4138bba8e3f228f1732b2deb49ab662f
srikasip <srikasip@gmail.com>
2016-12-22 14:20:57 -0500

    Added the templating for dynamic websites and set the layout for the canvas

--- DynamicHelper.py
+++ DynamicHelper.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup as bs
+
 class DynamicHelper:
   def __init__(self):
     self.initiated = True
@@ -5,7 +7,26 @@ class DynamicHelper:
   @staticmethod
   def GetDynamicContent(mainPath):
     if mainPath == "canvas":
-      data = "You're in the <strong>canvas</strong>!  We are working on it!\n"
+      with open('static/canvas/template.html') as mainCanvas:
+        templateSoup = bs(mainCanvas.read())
+      templateBlocks = templateSoup.find_all("template")
+      
+      for block in templateBlocks:
+        pageURL = block.get_text()
+        with open('static/canvas/'+pageURL) as blockPage:
+          pageSoup = bs(blockPage.read())
+        
+        block.parent.append(pageSoup.find("body"))
+        block.parent.find("body").unwrap()
+        block.decompose()
+
+        mainHead = templateSoup.find("head")
+        mainHead.append(pageSoup.find("head"))
+        mainHead.find("head").unwrap()
+
+
+
+      data = str(templateSoup.prettify())
       content_type = "text/html"
 
     else:
