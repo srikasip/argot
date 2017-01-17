8cb7d7a4cfabbd249508c4fa7df94dae089178ee
srikasip <srikasip@gmail.com>
2017-01-17 11:08:30 -0500

    Added the textbox edit stuff

---
+++ static/canvas/scripts/header.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  ////TODO: Get the actual name of the project and load in the textbox.
+  $("#txtProjectTitle").keypress(function(e){
+    if(e.which == 13)
+    {
+      $(this).blur();
+    }
+  });
+  $("#txtProjectTitle").blur(function(){
+    currentName = $(this).val().trim();
+    if(currentName == "")
+    {
+      $(this).val($(this).attr("default-text"));
+    }
+    //TODO: Call AJAX to actually save the name of the project.
+  });
+});
+
+//<h1 id="lblProjectTitle" class="title" default-text="Untitled App">Untitled App</h1>
